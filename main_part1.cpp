d8295389b8718f4e9eadff05846d1e0bdf866829
Bandekhoda223 <Emadmalekossadat@gmail.com>
2024-06-07 13:45:31 +0330

    settings, args and FileHash

--- main_part1.cpp
+++ main_part1.cpp
@@ -23,3 +23,42 @@ struct Task {
     bool isDirectory;
     struct Task* next;
 };
+
+
+struct FileHashCount {
+    char hash[MD5_DIGEST_LENGTH * 2 + 1];
+    int count;
+    struct FileHashCount* next;
+};
+
+struct FileTypeCount {
+    TCHAR extension[BUF_SIZE];
+    int count;
+};
+
+struct FileInfo filesMap[MAX_FILES];
+struct FileHashCount* fileHashCountHead = NULL;
+struct FileTypeCount fileTypeCounts[MAX_FILE_TYPES];
+CRITICAL_SECTION cs;
+int fileCount = 0;
+int fileTypeCountSize = 0;
+bool done = false;
+DWORD totalSizeBefore = 0;
+DWORD totalSizeAfter = 0;
+int duplicateFileCount = 0;
+TCHAR deletedFiles[MAX_FILES][MAX_PATH];
+int deletedFileCount = 0;
+
+unsigned __stdcall ProcessFile(void* data);
+DWORD WINAPI WorkerThread(LPVOID lpParam);
+void EnqueueTask(const TCHAR* path, bool isDirectory);
+bool DequeueTask(struct Task* task);
+void ProcessDirectory(const TCHAR* directoryPath);
+char* CalculateFileHash(const TCHAR* filePath);
+bool IsDuplicateFile(const char* hash, const TCHAR* filePath);
+void MarkFileAsProcessed(const char* hash, const TCHAR* filePath);
+void IncrementFileHashCount(const char* hash);
+void DecrementFileHashCount(const char* hash);
+void IncrementFileTypeCount(const TCHAR* extension);
+void PrintSummary();
+void WriteLog(const TCHAR* directoryPath, const TCHAR* message);
